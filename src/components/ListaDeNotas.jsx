c793932467222e57e7a18c7134e059db978cb091
Matheus Baldissara <baldissaramatheus@gmail.com>
2021-06-05 19:18:51 -0300

    Inclui importacao de css

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { CardNota } from './CardNota';
+import { CardNota } from './CardNota/CardNota';
 
 export class ListaDeNotas extends Component {
   render() {
     return (
       <ul>
-        { Array.of('Trabalho', 'Trabalho', 'Estudos').map(categoria => {
+        { Array.of('Trabalho', 'Trabalho', 'Estudos').map((categoria, i) => {
           return (
-            <li>
+            <li key={i}>
               <div>{categoria}</div>
               <CardNota />
             </li>
